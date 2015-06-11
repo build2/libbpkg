161d105204eb66148c28ef2615f1a12d3ba33136
Boris Kolpackov <boris@codesynthesis.com>
2015-06-11 17:11:54 +0200

    Manifest object model, take 1

---
+++ bpkg/manifest.cxx
@@ -0,0 +1,11 @@
+// file      : bpkg/manifest.cxx -*- C++ -*-
+// copyright : Copyright (c) 2014-2015 Code Synthesis Ltd
+// license   : MIT; see accompanying LICENSE file
+
+#include <bpkg/manifest>
+
+using namespace std;
+
+namespace bpkg
+{
+}
