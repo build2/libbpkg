621ce5fa18151a0cf25b4be6e35c8341f4d0fa12
Karen Arutyunov <karen@codesynthesis.com>
2019-01-16 20:47:30 +0300

    Update copyright year

--- libbpkg/package-name.hxx
+++ libbpkg/package-name.hxx
@@ -1,5 +1,5 @@
 // file      : libbpkg/package-name.hxx -*- C++ -*-
-// copyright : Copyright (c) 2014-2018 Code Synthesis Ltd
+// copyright : Copyright (c) 2014-2019 Code Synthesis Ltd
 // license   : MIT; see accompanying LICENSE file
 
 #ifndef LIBBPKG_PACKAGE_NAME_HXX
